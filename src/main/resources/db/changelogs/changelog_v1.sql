af2fad4e87ac47d34e00815b50c38615a5effaf7
Rohit Patil <rohitvpatil0810@gmail.com>
2024-11-12 23:50:40 +0530

    chore: minor changes and upgrade dependencies

--- src/main/resources/db/changelogs/changelog_v1.sql
+++ src/main/resources/db/changelogs/changelog_v1.sql
@@ -1,14 +1,1 @@
--- liquibase formatted sql
-
--- changeset manas.jadhav:1727813630278-1
-CREATE TABLE "users" ("id" UUID NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE, "email" VARCHAR(255) NOT NULL, "first_name" VARCHAR(255), "google_id" VARCHAR(255), "is_email_verified" BOOLEAN DEFAULT FALSE NOT NULL, "is_mobile_verified" BOOLEAN DEFAULT FALSE NOT NULL, "last_logout" TIMESTAMP WITH TIME ZONE, "lastname" VARCHAR(255), "mobile" VARCHAR(255) NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE, CONSTRAINT "users_pkey" PRIMARY KEY ("id"));
-
--- changeset manas.jadhav:1727813630278-2
-ALTER TABLE "users" ADD CONSTRAINT "uk63cf888pmqtt5tipcne79xsbm" UNIQUE ("mobile");
-
--- changeset manas.jadhav:1727813630278-3
-ALTER TABLE "users" ADD CONSTRAINT "uk6dotkott2kjsp8vw4d0m25fb7" UNIQUE ("email");
-
--- changeset manas.jadhav:1727813630278-4
-ALTER TABLE "users" ADD CONSTRAINT "ukovh8xmu9ac27t18m56gri58i1" UNIQUE ("google_id");
-
+-- empty Changelog
