43cc02d7a89f44c1f69c85551be00848f1c532d1
manasjadhav <manasjjadhav@gmail.com>
2024-12-12 02:06:24 +0530

    removed unnecessay imports

--- src/main/java/com/whytowait/api/v1/controllers/TimingController.java
+++ src/main/java/com/whytowait/api/v1/controllers/TimingController.java
@@ -4,10 +4,7 @@ import com.whytowait.api.common.exceptions.BadRequestException;
 import com.whytowait.api.common.responses.ApiResponse;
 import com.whytowait.api.common.responses.SuccessResponse;
 import com.whytowait.api.v1.services.TimingService;
-import com.whytowait.core.annotations.CheckMerchantManagerRole;
-import com.whytowait.core.annotations.enums.RequestSource;
 import com.whytowait.domain.dto.time.TimingReqDTO;
-import com.whytowait.domain.models.enums.MerchantRole;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +16,6 @@ public class TimingController {
     @Autowired
     TimingService timingService;
 
-    @CheckMerchantManagerRole(requiredAuthority = MerchantRole.MERCHANT_OWNER, source = RequestSource.BODY)
     @PostMapping(path = "/create")
     public ApiResponse createRestaurantTiming(@Valid @RequestBody TimingReqDTO requestDTO) throws BadRequestException {
         timingService.createRestaurantTiming(requestDTO);
